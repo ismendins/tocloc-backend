ad1d9ca30f109cff2e2153e11975e971f3fc9e4e
Isabella Mendes <isabellammartins7@gmail.com>
2024-12-06 04:34:04 -0300

    Commit inicial

---
+++ src/test/java/tocloc/tocloc/ToclocApplicationTests.java
@@ -0,0 +1,13 @@
+package tocloc.tocloc;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ToclocApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
